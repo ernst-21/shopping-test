3b78a01ea1c2f2480101d9168392e215c9a1b789
Ernesto Rivera <riverag8504@gmail.com>
2022-08-08 11:52:49 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Box} from "@mui/material";
+import './App.css';
+
+function App() {
+
+  return (
+    <Box className="App">
+      Test
+    </Box>
+  );
+}
+
+export default App;
